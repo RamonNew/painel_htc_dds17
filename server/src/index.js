97844b653d5140e6f04302b44fb75e8d80456370
RAMON DE HOLANDA NASCIMENTO <rholanda@labcetec.net>
2024-10-21 21:42:19 -0300

    Finalizando CRUD READ,UPDATE E DELETE

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 //Importando pacote express
 import express from 'express';
 
-import { cadastroAula } from './controllers/AulaController.js';
+import { cadastroAula,mostrandoAulas, atualizandoAula, excluindoAula } from './controllers/AulaController.js';
 
 //Intanciando objeto express
 const app = express();
@@ -17,24 +17,9 @@ app.get('/', (req, res) => {
 
 //Rotas de CRUD de aulas
 app.post('/aulas',cadastroAula);
-
-app.get('/aulas',(req,res)=>{
-    res.status(200).json(
-        [
-            {
-              "data": "2024-10-07T00:00:00.000Z",
-              "data_hora_inicio": "2024-10-07T18:30:00.000Z",
-              "data_hora_fim": "2024-10-07T22:00:00.000Z",
-              "turma": "EMP-NBM-03",
-              "instrutor": "JOEL",
-              "unidade_curricular": "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
-              "ambiente": "VTRIA-3-SALA-3004",
-              "chave": null,
-              "id": "1300"
-            }
-        ]
-    );
-})
+app.get('/aulas',mostrandoAulas);
+app.put('/aulas/:id',atualizandoAula);
+app.delete('/aulas/:id',excluindoAula);
 
 app.listen(porta, () => {
   console.log(`API funcionando na porta ${porta}`);
