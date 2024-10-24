d74aae574a59a815486925606cccc269da70fbeb
RAMON DE HOLANDA NASCIMENTO <rholanda@labcetec.net>
2024-10-23 21:03:54 -0300

    Trabalhando com status web, mensagens personalizadas

--- server/src/controllers/AulaController.js
+++ server/src/controllers/AulaController.js
@@ -1,23 +1,35 @@
-import { createAula,readAulas,updateAula,deleteAula } from '../models/AulaModel.js';
+import {
+  createAula,
+  readAulas,
+  updateAula,
+  deleteAula,
+  getOneAula,
+} from '../models/AulaModel.js';
+import { isNullOrEmpty, validateAula } from '../validations/AulaValidation.js';
 
 export async function cadastroAula(req, res) {
   console.log('AulaController cadastroAula');
   //Criando constante aula
   const aula = req.body;
-  try {
-    //Declarando status com o codigo da resposta e reposta como JSON
-    const [status, resposta] = await createAula(aula);
-    res.status(status).json(resposta);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
+
+  if (validateAula(aula)) {
+    res.status(400).json({ message: 'Aula não pode conter campos vazios' });
+  } else {
+    try {
+      //Declarando status com o codigo da resposta e reposta como JSON
+      const [status, resposta] = await createAula(aula);
+      res.status(status).json(resposta);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
   }
 }
 
-export async function mostrandoAulas(req,res) {
+export async function mostrandoAulas(req, res) {
   console.log('AulaController mostrandoAulas');
   try {
-    const[status,resposta] = await readAulas();
+    const [status, resposta] = await readAulas();
     res.status(status).json(resposta);
   } catch (error) {
     console.log(error);
@@ -25,29 +37,54 @@ export async function mostrandoAulas(req,res) {
   }
 }
 
-export async function atualizandoAula(req,res) {
+export async function atualizandoAula(req, res) {
   console.log('AulaController atualizandoAula');
-  const {id} = req.params;
+  const { id } = req.params;
   const aula = req.body;
 
-  try {
-    const [status,resposta] = await updateAula(aula,id);
-    res.status(status).json(resposta);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
+  if (validateAula(aula) || isNullOrEmpty(id)) {
+    res.status(400).json({ message: 'Aula não pode conter campos vazios' });
+  } else {
+    try {
+      const [status, resposta] = await updateAula(aula, id);
+      res.status(status).json(resposta);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
   }
 }
 
-export async function excluindoAula(req,res) {
+export async function excluindoAula(req, res) {
   console.log('AulaController excluindoAula');
-  const {id} = req.params;
+  const { id } = req.params;
 
-  try {
-    const [status,resposta] = await deleteAula(id);
-    res.status(status).json(resposta);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
+  if (isNullOrEmpty(id)) {
+    res.status(400).json({ message: 'O id deve ser preenchido' });
+  } else {
+    try {
+      const [status, resposta] = await deleteAula(id);
+      res.status(status).json(resposta);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  }
+}
+
+export async function mostrandoUmaAula(req, res) {
+  console.log('AulaController mostrandoUmaAula');
+  const { id } = req.params;
+
+  if (isNullOrEmpty(id)) {
+    res.status(400).json({ message: 'O id deve ser preenchido' });
+  } else {
+    try {
+      const [status, resposta] = await getOneAula(id);
+      res.status(status).json(resposta);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
   }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,10 @@
 //Importando pacote express
 import express from 'express';
+//Importando cors
+import cors from 'cors';
 
-import { cadastroAula,mostrandoAulas, atualizandoAula, excluindoAula } from './controllers/AulaController.js';
+
+import { cadastroAula,mostrandoAulas, atualizandoAula, excluindoAula, mostrandoUmaAula } from './controllers/AulaController.js';
 
 //Intanciando objeto express
 const app = express();
@@ -9,6 +12,9 @@ const porta = 5000;
 
 //Configurando comunicação em JSON
 app.use(express.json());
+
+//Permitindo acesso do front ao backend atraves do localhost
+app.use(cors());
 
 //Rota de teste da API
 app.get('/', (req, res) => {
@@ -18,6 +24,7 @@ app.get('/', (req, res) => {
 //Rotas de CRUD de aulas
 app.post('/aulas',cadastroAula);
 app.get('/aulas',mostrandoAulas);
+app.get('/aulas/:id',mostrandoUmaAula);
 app.put('/aulas/:id',atualizandoAula);
 app.delete('/aulas/:id',excluindoAula);
 
